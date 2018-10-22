56ad1751537c5c6091d1c0620f376e66b60dc203
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-22 09:55:00 -0700

    assignment prompt menu completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -7,7 +7,7 @@ module.exports = class MenuController {
         type: "list",
         name: "mainMenuChoice",
         message: "Please choose from an option below: ",
-        choices: ["Add new contact", "Exit"]
+        choices: ["Add new contact", "Get current date and time", "Exit"]
       }
     ];
     this.contacts = [];
@@ -21,6 +21,9 @@ module.exports = class MenuController {
         switch (response.mainMenuChoice) {
           case "Add new contact":
             this.addContact();
+            break;
+          case "Get current date and time":
+            this.getDate();
             break;
           case "Exit":
             this.exit();
@@ -41,6 +44,13 @@ module.exports = class MenuController {
   addContact() {
     this.clear();
     console.log("addContact called");
+    this.main();
+  }
+
+  getDate() {
+    this.clear();
+    let date = new Date();
+    console.log(date.toLocaleString("en-US", { hour12: true }));
     this.main();
   }
 
